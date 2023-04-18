3f9f05f15f621f7a48b530cdc085fcdff70d4b2a
Ilya Miloshevich <imiloshevich@alfabank.ru>
2023-04-19 03:12:00 +0400

    change styles and add data files

---
+++ docs-src/_data/contacts.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'email',
+    value: 'mailto:ilyamilosevic@gmail.com',
+    title: 'ilyamilosevic@gmail.com',
+  },
+  {
+    type: 'github',
+    value: '//github.com/ilyamilosevic',
+    title: 'github.com/ilyamilosevic',
+  },
+  {
+    type: 'linkedin',
+    value: '//www.linkedin.com/in/ilyamilosevic/',
+    title: 'linkedin.com/in/ilyamilosevic/',
+  },
+];
---
+++ docs-src/_data/jobs.js
@@ -0,0 +1,117 @@
+module.exports = [
+  {
+    company: {
+      name: 'Avito',
+      description: 'biggest classifieds site in the world',
+    },
+    overallInterval: 'march 2022 – present',
+    roles: [
+      {
+        name: 'Senior front-end engineer',
+        description: 'cart team',
+        achievements: [
+          `
+            Developed a&nbsp;shopping cart checkout page for two platforms
+            (desktop and mobile) using React, Redux and&nbsp;CSS modules,
+            which enabled the launch of&nbsp;the&nbsp;world&rsquo;s first
+            classifieds with a&nbsp;shopping cart
+        `,
+          `
+            Developed a&nbsp;user&rsquo;s stock page with an&nbsp;infinite
+            list of&nbsp;products
+        `,
+        ],
+      },
+    ],
+  },
+  {
+    company: {
+      name: 'Alfa-bank',
+      description: 'largest of the private banks in Russia',
+    },
+    overallInterval: 'march 2019 – june 2021',
+    roles: [
+      {
+        name: 'Front-end tech lead',
+        description: 'credit team',
+        interval: 'january 2021 – june 2021',
+        achievements: [
+          `
+            Was the technical lead in&nbsp;the&nbsp;team
+            of&nbsp;2&nbsp;front-end developers, directed the formation and
+            prioritization of&nbsp;a&nbsp;backlog of&nbsp;20+&nbsp;tasks
+            using Agile methodology, made architectural decisions, and
+            monitored the team&rsquo;s performance, resulting
+            in&nbsp;successful delivery of&nbsp;2&nbsp;main projects
+        `,
+        ],
+      },
+      {
+        name: 'Senior front-end engineer',
+        description: 'SME team',
+        interval: 'march 2019 – january 2021',
+        achievements: [
+          `
+            Built a&nbsp;tariff picker using React and CSS animations,
+            streamlining the process of&nbsp;selecting tariffs and resulting
+            in&nbsp;a&nbsp;90% reduction in&nbsp;selection time
+        `,
+          `
+            Managed the development of&nbsp;the form builder. Wrote&nbsp;70%
+            of&nbsp;the code, reviewed the remaining 30%. Reduced the amount
+            of&nbsp;code by&nbsp;2&nbsp;times compared to&nbsp;the old
+            version and reduced the release cycle from a&nbsp;week
+            to&nbsp;several hours, transferring layout and&nbsp;form content
+            control to&nbsp;content management team
+        `,
+          `
+            Built dozens of&nbsp;highly interactive pages, adding new
+            components to&nbsp;the design system using advanced approaches
+            such as&nbsp;compound components, React Context, portals
+            and&nbsp;centralized management of&nbsp;z-index
+        `,
+        ],
+      },
+    ],
+  },
+  {
+    company: {
+      name: 'LitRes',
+      description: 'produces and distributes e-books and digital audiobooks',
+    },
+    overallInterval: 'august 2017 – march 2019',
+    roles: [
+      {
+        name: 'Front-end engineer',
+        description: 'front-end team',
+        achievements: [
+        `
+          Built an&nbsp;entire front-end of&nbsp;the map widget for book
+          libraries, which is&nbsp;embedded in&nbsp;dozens of&nbsp;pages
+          of&nbsp;LitRes internal and external projects. Provided
+          30&nbsp;&mdash; 40% lightness of&nbsp;the widget compared
+          to&nbsp;similar solutions
+        `,
+          `
+          Implemented a&nbsp;modern design system
+          on&nbsp;a&nbsp;React-based stack, showcased its value
+          to&nbsp;senior management, and persuaded them to&nbsp;transition
+          all main projects, centralizing development and deduplicating
+          code by&nbsp;2&nbsp;times
+        `,
+        `
+          Was the main contributor to&nbsp;the web version of&nbsp;the
+          book reading application: migrated the application
+          to&nbsp;a&nbsp;new API version for saving quotes and supported
+          the new FB3 format
+        `,
+        `
+          Developed complex pages and widgets such as&nbsp;a&nbsp;page for
+          viewing all books with multiple filters and a&nbsp;widget for
+          ratings and comments
+        `,
+        ],
+      },
+    ],
+  },
+];
